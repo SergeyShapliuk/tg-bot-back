b90b03194948296ba454946123ef73eb85409315
shapl_xlbobtq <sergeshapluk@gmail.com>
2024-08-28 17:01:12 +0300

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, {polling: true});
+
+const start_bot = () => {
+    bot.setMyCommands([
+        {command: 'start', description: 'Запуск бота'},
+    ]).then(res => console.log('res', res))
+
+    bot.on('message', async msg => {
+        const text = msg.text
+        const chat_id = msg.chat.id
+
+        if (text === '/start') {
+            await bot.sendSticker(chat_id, `https://data.chpic.su/stickers/h/HelloDigitalWorld/HelloDigitalWorld_001.webp?v=1709113684`)
+            await bot.sendMessage(chat_id, `Добро пожаловать ${msg.from.first_name}`, {
+                reply_markup: {
+                    keyboard: [
+                        [{
+                            text: 'Мое портфолио',
+                            web_app: {url: 'https://sergeyshapliuk.github.io/portfolio/'}
+                        }, {text: 'Мини игра',web_app:{url: 'https://play.famobi.com/bubble-tower-3d'}}]
+                    ], resize_keyboard: true
+                }
+            })
+        }
+        if (text === '/portfolio') {
+            await bot.sendMessage(chat_id, `https://sergeyshapliuk.github.io/portfolio/`)
+        }
+        console.log(msg)
+    })
+}
+
+start_bot()
